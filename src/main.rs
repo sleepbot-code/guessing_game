710bdf1f175ced7e93d4e3a63c414f0fcf86de31
sleepbot-code <78042190+sleepbot-code@users.noreply.github.com>
2022-07-15 21:21:33 +0530

    fixing oopsie and adding new features

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 
-fn matching(guessed: u32, secret_number: u32){
+fn matching(guessed: u8, secret_number: u8){
     match guessed.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
@@ -13,17 +13,19 @@ fn matching(guessed: u32, secret_number: u32){
 
 
 fn main() {
+    let mut guesses: Vec<u8> = Vec::new();
+    let secret_number: u8 = rand::thread_rng().gen_range(1..=100);
     loop{
         println!("Guess: ");
-        let secret_number = rand::thread_rng().gen_range(1..=10);
         let mut guess = String::new();
-
         io::stdin().read_line(&mut guess).expect("Fail");
-        let guess: u32 = guess.trim().parse().expect("Enter number!");
-
+        let guess: u8 = guess.trim().parse().expect("Enter number!");
+        guesses.push(guess);
         matching(guess, secret_number);
         if guess == secret_number{
             break;
         }
     }
+    println!("Your Guesses are: {:?}", guesses);
+    println!("Number of Guesses: {}", guesses.len())
 }
