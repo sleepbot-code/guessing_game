419f65a098f5292b0ebd1641ac73c79884957390
sleepbot-code <neeladrijlt@gmail.com>
2022-07-16 07:55:33 +0530

    Poof

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,20 @@ fn matching(guessed: u8, secret_number: u8){
 
 
 fn main() {
-    let mut guesses: Vec<u8> = Vec::new();
+    let mut guesses: Vec<String> = Vec::new();
     let secret_number: u8 = rand::thread_rng().gen_range(1..=100);
+    println!("All guesses in range 1 to 100, both inclusive");
     loop{
-        println!("Guess in range 1 to 100, both inclusive: ");
+        println!("Guess: ")
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Fail");
         let guess: u8 = guess.trim().parse().expect("Enter number!");
-        guesses.push(guess);
+        guesses.push(guess.to_string());
         matching(guess, secret_number);
         if guess == secret_number{
             break;
         }
     }
-    println!("Your Guesses are: {:?}", guesses);
+    println!("Your Guesses are: {:?}", guesses.join(", "));
     println!("Number of Guesses: {}", guesses.len());
 }
